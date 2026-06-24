e7ba30cdad7190557d0989e6e9bb4f96f210d969
Andrey Piroženok <a.pirozenoks@dynatech.lv>
2026-06-24 13:54:06 +0300

    feat: dino boss attacks

--- scenes/enemy/dinosaurik/dinosaurik.gd
+++ scenes/enemy/dinosaurik/dinosaurik.gd
@@ -1,12 +1,6 @@
 extends CharacterBody2D
 
 
-@export var walk_speed: float = 300.0
-@export var walk_min_time: float = 1.5
-@export var walk_max_time: float = 5.0
-
-@export var charge_speed: float = 1000.0
-@export var charge_decel: float = 200.0
 
 # signal expected by Exit
 signal got_lobotomized
@@ -21,15 +15,56 @@ enum State {
 	METEOR_TAIL,
 }
 
+enum ChargePhase {
+	TELEGRAPH,
+	CHARGE,
+	RECOVERY,
+}
+
+@export var contact_damage: int = 20
+@export var contact_knockback: float = 300.0
+
+@export var walk_speed: float = 300.0
+@export var walk_min_time: float = 1.5
+@export var walk_max_time: float = 5.0
+
+@export var charge_speed: float = 1000.0
+@export var charge_decel: float = 200.0
+
+@export var run_to_center_speed: float = 450.0
+@export var center: Node2D
+@export var meteor_manager: MeteorManager
+@export var meteor_rect: Control
+
+@export var shower_meteor_interval: float = 0.1
+@export var shower_meteors_min: int = 40
+@export var shower_meteors_max: int = 120
+
+@export var tail_meteor_interval: float = 0.5
+@export var tail_meteors_min: int = 10
+@export var tail_meteors_max: int = 20
+
+var _states: Array[State]
+var _prev_state := State.STARTING
+var _state_just_changed: bool
+
+var _walk_time: float = 0.0
+
+var _charge_phase: ChargePhase
+
+var _meteors_left: int = 0
+var _meteor_cooldown: float = 0
+
 @onready var cnb := ChainAndBalls.get_instance()
 @onready var health_component: HealthComponent = $HealthComponent
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var sprite: Sprite2D = %Sprite2D
 @onready var flip_group: Node2D = $FlipGroup
+@onready var hurtbox: Area2D = $HurtBox
 
-var _states: Array[State]
-var _prev_state := State.STARTING
-var _state_just_changed: bool
+
+func _ready() -> void:
+	hurtbox.body_entered.connect(_hurtbox_body_entered)
 
 
 func _fill_states() -> void:
@@ -72,8 +107,6 @@ func _physics_process(_delta: float) -> void:
 
 
 
-var _walk_time: float = 0.0
-
 func _do_walk() -> void:
 	if _state_just_changed:
 		_walk_time += randf_range(walk_min_time, walk_max_time)
@@ -93,13 +126,6 @@ func _do_walk() -> void:
 		_state_finished()
 
 
-enum ChargePhase {
-	TELEGRAPH,
-	CHARGE,
-	RECOVERY,
-}
-
-var _charge_phase: ChargePhase
 
 func _do_charge() -> void:
 	if _state_just_changed:
@@ -137,8 +163,59 @@ func _start_charge() -> void:
 	_force_play(&"charge")
 
 
-func _do_meteor_shower() -> void: _state_finished()
-func _do_meteor_tail() -> void: _state_finished()
+func _do_meteor_shower() -> void:
+	if _state_just_changed:
+		_meteors_left = randi_range(shower_meteors_min, shower_meteors_max)
+
+	_do_meteors(shower_meteor_interval, _get_random_arena_pos)
+
+
+func _do_meteor_tail() -> void:
+	if _state_just_changed:
+		_meteors_left = randi_range(tail_meteors_min, tail_meteors_max)
+
+	_do_meteors(tail_meteor_interval, _get_player_pos)
+
+
+func _do_meteors(interval: float, get_spawn_pos: Callable) -> void:
+	if _state_just_changed:
+		_meteor_cooldown = 2.0
+
+	if not _is_in_center():
+		_run_to_center()
+		return
+
+	_look_at_player()
+	animation_player.play(&"charge", 0.5)
+
+	var delta := get_physics_process_delta_time()
+	_meteor_cooldown -= delta
+
+	if _meteor_cooldown <= 0:
+		var pos: Vector2 = get_spawn_pos.call()
+		meteor_manager.do_meteor(pos)
+
+		_meteor_cooldown = interval
+		_meteors_left -= 1
+
+	if _meteors_left <= 0:
+		_state_finished()
+
+
+func _is_in_center() -> bool:
+	const D: float = 40
+
+	return global_position.distance_squared_to(center.global_position) <= D*D
+
+
+func _run_to_center() -> void:
+	animation_player.play(&"walk", 0.5)
+
+	var dir := global_position.direction_to(center.global_position)
+	velocity = dir * run_to_center_speed
+	_look_in(dir.x)
+
+	move_and_slide()
 
 
 func _look_at_player() -> void:
@@ -155,3 +232,42 @@ func _force_play(anim: StringName) -> void:
 	if animation_player.current_animation == anim:
 		animation_player.stop()
 	animation_player.play(anim, 0.5)
+
+
+
+func _get_player_pos() -> Vector2:
+	return cnb.player.global_position
+
+
+
+func _get_random_arena_pos() -> Vector2:
+	var r: Rect2 = meteor_rect.get_global_rect()
+
+	return r.position + Vector2(
+		randf_range(0, r.size.x),
+		randf_range(0, r.size.y),
+	)
+
+
+func _hurtbox_body_entered(node: Node2D) -> void:
+	if health_component.is_dead():
+		return
+
+	if node != cnb.player:
+		return
+
+	var dir := global_position.direction_to(node.global_position)
+	if not velocity.is_zero_approx():
+		dir = (dir + velocity.normalized()) * 0.5
+
+	cnb.health_component.damage(contact_damage)
+	cnb.player.apply_central_impulse(dir * contact_knockback)
+
+
+
+func apply_knockback(knockback: Vector2) -> void:
+	print("knockback!")
+
+
+func apply_armour(flail: RigidBody2D, knockback: Vector2) -> void:
+	print("armour!")
