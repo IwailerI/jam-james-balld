211cdea8e0d3b31b80c7a8b163c8c4f56dec2ca8
zhizhoid <misha2006.mk@gmail.com>
2026-06-24 23:31:48 +0300

    fixed bullets disappearing immediately when muzzle is in a wall

--- scenes/bullet/bullet.gd
+++ scenes/bullet/bullet.gd
@@ -8,23 +8,35 @@ extends Area2D
 @onready var cnb := ChainAndBalls.get_instance()
 @onready var life_timer: Timer = $LifeTimer
 
-var direction: Vector2
-
+var _direction: Vector2
+var _initial_player_pos: Vector2
 
 func _ready() -> void:
 	body_entered.connect(_on_body_entered)
 	life_timer.timeout.connect(queue_free)
 
-	direction = global_position.direction_to(cnb.player.global_position)
-	look_at(cnb.player.global_position)
+	_initial_player_pos = cnb.player.global_position
+
+	_direction = global_position.direction_to(_initial_player_pos)
+	look_at(_initial_player_pos)
 
 
 func _process(delta: float) -> void:
-	position += direction * speed * delta
+	position += _direction * speed * delta
+
+
+func _handle_obstacle_collision() -> void:
+	# ignore collisions with obstacles that happen before reaching the initial player position
+	# this is needed to avoid the bullet disappearing immediately if it is instantiated inside a collider
+	if _direction.dot(_initial_player_pos - global_position) <= 0:
+		queue_free()
 
 
 func _on_body_entered(area: Node2D) -> void:
-	if area == cnb.player:
-		cnb.health_component.damage(damage)
-		cnb.player.apply_central_impulse(global_position.direction_to(cnb.player.global_position) * knockback)
+	if area != cnb.player:
+		_handle_obstacle_collision()
+		return
+
+	cnb.health_component.damage(damage)
+	cnb.player.apply_central_impulse(global_position.direction_to(cnb.player.global_position) * knockback)
 	queue_free()
