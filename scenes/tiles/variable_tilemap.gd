74344251627befb805a0abb6d81b5b05b4409308
zhizhoid <misha2006.mk@gmail.com>
2026-06-24 17:57:54 +0300

    variable_tilemap improvement

--- scenes/tiles/variable_tilemap.gd
+++ scenes/tiles/variable_tilemap.gd
@@ -5,6 +5,8 @@ extends TileMapLayer
 
 
 func _ready() -> void:
+    tile_set = tile_set.duplicate()
+
     for src_id: int in custom_textures.keys():
         var texture := custom_textures[src_id]
         print("%s: swapping source %s texture to %s" % [name, src_id, texture.resource_name])
